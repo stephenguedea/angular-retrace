865da9aae9a643db2fd8eef72e1eca10e3cbe149
Stephen Ray Guedea <sguedea@appddictionstudio.com>
2021-06-09 17:23:18 -0500

    made a GET request method from service to json server api / data still renders

--- src/app/services/task.service.ts
+++ src/app/services/task.service.ts
@@ -1,3 +1,4 @@
+import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable, of } from 'rxjs';
 import { TASKS } from '../mock-tasks';
@@ -7,8 +8,9 @@ import { Task } from '../models/Task';
   providedIn: 'root'
 })
 export class TaskService {
+  private apiUrl = "http://localhost:5000/tasks";
 
-  constructor() { }
+  constructor(private http: HttpClient) { }
 
   // pass directive "method"
   // getTasks() : Task[] {
@@ -16,8 +18,13 @@ export class TaskService {
   // }
 
   // observable method for async data
+  // getTasks(): Observable<Task[]> {
+  //   return of(TASKS);
+  // }
+
+  // getting tasks data from json server 
   getTasks(): Observable<Task[]> {
-    return of(TASKS);
+    return this.http.get<Task[]>(this.apiUrl);
   }
 
 }
